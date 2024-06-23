0cf84c8c451a88fef616ce96d6241f821054f0a8
SW TanTran <102936001+tantransw@users.noreply.github.com>
2024-06-23 14:24:18 +0700

    [ptuanvu23] commit

---
+++ src/ss3_java_overview/HelloWorld.java
@@ -0,0 +1,11 @@
+package ss3_java_overview;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        char a = 'a';
+        char b = a;
+        char c = b;
+            
+        System.out.println(a=c);
+    }
+}
